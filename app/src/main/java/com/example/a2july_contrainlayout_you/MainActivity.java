ac43500f54eb622f1e999a3bfdaab3bfb54d56e4
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-07-04 00:12:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a2july_contrainlayout_you/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2july_contrainlayout_you;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
